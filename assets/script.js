c73c90a1aba174580f7e7d08caad8ddd1eaa485f
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-01-08 17:46:50 -0500

    Add a study topic randomiser.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,29 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var rgTopics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var topic;
+var randomTopic = rgTopics[Math.floor(Math.random() * rgTopics.length)];
 
-for (x=0; x<topics.length; x++) {
-console.log(topics[x]);
+console.log('Here are the topics we learned through Prework:');
+ListTopics();
+SelectTopic();
+
+
+
+function ListTopics(){
+for (x=0; x<rgTopics.length; x++) {
+console.log(rgTopics[x]);
 }
+}
+
+function SelectTopic() {
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
